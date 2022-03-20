b310ca748ce11d748f559fb85e89f25eec0a74d8
YounSeoYeon <syyoon1122@gmail.com>
2022-03-20 17:59:26 +0900

    서연-qna게시판 만드는중

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import AxiosSpring from './components/AxiosSpring';
+import QnaList from './components/sun/QnaList';
+import Sun from './components/sun/Sun';
 
 function App() {
   return (
     <div className="App">
       <AxiosSpring></AxiosSpring>
+      <hr/>
+      <Sun></Sun>
+      <hr/>
+      <QnaList></QnaList>
     </div>
   );
 }
