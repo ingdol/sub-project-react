de945e78519d049cebfd1c6c8203429d571ae182
YounSeoYeon <syyoon1122@gmail.com>
2022-03-23 14:31:48 +0900

    qna게시판 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import QnaList from './components/sun/QnaList';
-import Sun from './components/sun/Sun';
 import Navbar from './components/main/Navbar';
 import Main from './components/main/Main';
 import React from 'react';
@@ -7,8 +6,11 @@ import './App.css';
 import { Link, Routes, Route } from 'react-router-dom';
 import Intro from './components/Create/intro';
 import QnaTop from './components/sun/QnaTop';
-import SpaceClassList from './components/bch/SpaceClassList';
 
+import QnaInsert from './components/sun/QnaInsert';
+import QnaDetailView from './components/sun/QnaDetailView';
+import QnaUpdate from './components/sun/QnaUpdate';
+import SpaceClassList from './components/bch/SpaceClassList';
 
 function App() {
   return (
@@ -16,11 +18,20 @@ function App() {
       <div className="Nav">
         <Navbar />
       </div>
+      
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/QnaTop" element={<QnaTop />} />
         <Route path="/Intro" element={<Intro />} />
-        <Route path="/SpaceClassList" element={<SpaceClassList />} />
+        <Route path="/SpaceClassList" element={<SpaceClassList/>} />
+      </Routes>
+
+      {/* 서연 Routes */}
+      <Routes>
+        <Route path="/qnalist" element={<QnaList/>} />
+        <Route path="/qnaInsert" element={<QnaInsert />} />
+        <Route path="/qnadetailview/:hostqnaNo" element={<QnaDetailView></QnaDetailView>} />
+        <Route path="/qnaupdate/:hostqnaNo" element={<QnaUpdate></QnaUpdate>} />
       </Routes>
     </div>
   );
