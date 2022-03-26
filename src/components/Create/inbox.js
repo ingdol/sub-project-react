b6f222fcc4b4c142e4d1c9c053c6499a314cd31e
LeeDhffff <ehdgjs1311@naver.com>
2022-03-26 20:15:55 +0900

    커미잇2!!!!!!!

--- src/components/Create/inbox.js
+++ src/components/Create/inbox.js
@@ -1,26 +1,89 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
 import './Class2.css';
 import './box.css'
-function inbox(props) {
+import axios from 'axios';
+
+function Inbox(props) {
+    // state
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    // 서버에 요청해서 데이터 받아옴
+    // state 값 저장
+    const loadData = async () => {
+        setLoading(true);
+        const response = await axios.get('http://localhost:8080/HostCheck');
+        console.log(response.data);
+        setData(response.data);
+        setLoading(false);
+    }
+
+    const postSession = async () => {
+        setLoading(true);
+        const response = await axios.get('http://localhost:8080/SessionFourList');
+    }
+
+    // 렌더링할 때마다 호출 
+    // 빈배열 : loadData() 한 번만 호출
+    useEffect(() => {
+        axios.post(
+            'http://localhost:8080/SessionFourList',
+            {
+                sNick: window.sessionStorage.getItem("sessionNick")
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log('error : ', error.response)
+            });
+        setTimeout(() => {
+            loadData()
+        }, 100);
+        // loadData();
+    }, []);
+
+    const oncheck = (e) => {
+        if (data === 0) {
+            alert("호스트 권한이 필요합니다.");
+            window.location.replace("/hostsign");
+        }
+        else {
+            window.location.replace("/CreateClass");
+        }
+    }
+    const oncheck2 = (e) => {
+        if (data === 0) {
+            alert("호스트 권한이 필요합니다.");
+            window.location.replace("/hostsign");
+        }
+        else {
+            window.location.replace("/CreateSpace");
+        }
+    }
+
+
     return (
 
         <div className="inbox">
-            <table border="0" border-spacing="30px">
-                <tr><Link to="/CreateClass"><td><div className="inbox-2">
-                    <h2>클래스 개설</h2><br />
-                    <h3>클래스를 만들어보세요</h3>
-                    클래스를 만들어 당신이 만든<br />
-                    멋진 강의를 많은 사람들에게 보여주세요!<br /><br />
-                    ＊호스트 권한이 필요합니다.
-                </div></td></Link>
-                    <Link to="/CreateSpace"><td><div className="inbox-2">
-                        <h2>공간 개설</h2><br />
-                        <h3>당신의 공간을 등록해보세요.</h3><br />
+            <table border="0" className="tabler">
+                <tr><td width="400px">
+                    <button className="inbox-2" onClick={oncheck}>
+                        <h1>클래스 개설</h1><br />
+                        <h2>클래스를 만들어보세요</h2>
+                        클래스를 만들어 당신이 만든<br />
+                        멋진 강의를 많은 사람들에게 보여주세요!<br /><br />
+                        ＊호스트 권한이 필요합니다.
+                    </button></td>
+                    <td width="400px"><button className="inbox-2" onClick={oncheck2}>
+                        <h1>공간 개설</h1><br />
+                        <h2>당신의 공간을 등록해보세요.</h2>
                         당신이 소유한 공간을 소개하고<br />
-                        많은 사람들이 이용할 수 있도록 해보세요!<br />
-                    </div>
-                    </td></Link></tr>
+                        많은 사람들이 이용할 수 있도록 해보세요!<br /><br />
+                        ＊호스트 권한이 필요합니다.
+                    </button>
+                    </td></tr>
             </table>
 
         </div >
@@ -29,4 +92,4 @@ function inbox(props) {
     );
 }
 
-export default inbox;
+export default Inbox;
